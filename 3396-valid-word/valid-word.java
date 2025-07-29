7fbaa3e4ebe8517a672c3bf916fa12b66521d43c
Shruti Jaiswal <97962075+ShrutiJaiswal22@users.noreply.github.com>
2025-07-29 21:20:01 +0530

    Time: 2 ms (24.93%) | Memory: 41.7 MB (98.61%) - LeetSync

---
+++ 3396-valid-word/valid-word.java
@@ -0,0 +1,31 @@
+// class Solution {
+//     public boolean isValid(String word) {
+        
+//     }
+// }
+
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) return false;
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for (char ch : word.toCharArray()) {
+            if (!Character.isLetterOrDigit(ch)) {
+                return false; // Invalid character
+            }
+
+            if (Character.isLetter(ch)) {
+                char lower = Character.toLowerCase(ch);
+                if ("aeiou".indexOf(lower) >= 0) {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+}
