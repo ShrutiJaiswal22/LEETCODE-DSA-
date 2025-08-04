3c6ffd849f054e2a0290384637a7420b7ec09717
Shruti Jaiswal <97962075+ShrutiJaiswal22@users.noreply.github.com>
2025-08-04 22:25:36 +0530

    Time: 50 ms (59.78%) | Memory: 55.2 MB (62.92%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -0,0 +1,34 @@
+// class Solution {
+//     public int totalFruit(int[] fruits) {
+        
+//     }
+// }
+
+import java.util.*;
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        Map<Integer, Integer> basket = new HashMap<>();
+        int maxFruits = 0;
+        int start = 0;
+
+        for (int end = 0; end < fruits.length; end++) {
+            int fruit = fruits[end];
+            basket.put(fruit, basket.getOrDefault(fruit, 0) + 1);
+
+            // If more than 2 types, shrink window from start
+            while (basket.size() > 2) {
+                int startFruit = fruits[start];
+                basket.put(startFruit, basket.get(startFruit) - 1);
+                if (basket.get(startFruit) == 0) {
+                    basket.remove(startFruit);
+                }
+                start++;
+            }
+
+            maxFruits = Math.max(maxFruits, end - start + 1);
+        }
+
+        return maxFruits;
+    }
+}
